05e2ae2f108ca3474230de73a7eef128eeb512b7
olegivo <olegivo@gmail.com>
2020-11-18 11:40:04 +0300

    Stage 3/6: Sorting it out

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -4,6 +4,12 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val paramByCommand = Params.parse(args)
+
+    if (paramByCommand.containsKey(Params.SortIntegers)) {
+        SortIntegersTool().sort()
+        return
+    }
+
     val sortingTool = SortingTool.get(paramByCommand[Params.DataType])
     sortingTool.read()
     sortingTool.printResult()
--- Sorting Tool/task/src/sorting/Params.kt
+++ Sorting Tool/task/src/sorting/Params.kt
@@ -1,14 +1,19 @@
 package sorting
 
-enum class Params(val key: String) {
-    DataType("-dataType");
+enum class Params(val key: String, val containsValue: Boolean) {
+    DataType("-dataType", true),
+    SortIntegers("-sortIntegers", false);
 
     companion object {
-        fun parse(args: Array<String>): Map<Params, String> {
+        fun parse(args: Array<String>): Map<Params, String?> {
             val commands = values().associateBy { it.key }
             return args.mapIndexed { index, s -> index to s }
                 .filter { commands.containsKey(it.second) }
-                .associate { commands.getValue(it.second) to args[it.first + 1] }
+                .associate {
+                    val param = commands.getValue(it.second)
+                    val value = if (param.containsValue) args[it.first + 1] else null
+                    param to value
+                }
         }
     }
 }
